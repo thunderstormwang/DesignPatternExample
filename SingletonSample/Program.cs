bc14e9833c727d88b6b0534dd10f102248a2a78a
thunderstormwang <thunderstormwang@gmail.com>
2018-07-30 21:38:24 +0800

    Add Singleton

---
+++ SingletonSample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonSample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                SingletonClass.SingletonObject.Show();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SingletonSample/SingletonClass.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace SingletonSample
+{
+    /// <summary>
+    /// sealed class
+    /// </summary>
+    public sealed class SingletonClass
+    {
+        private int i = 0;
+
+        private static SingletonClass _singletonObject;
+        private static object _syncRoot = new object();
+
+        /// <summary>
+        /// private constructor
+        /// </summary>
+        private SingletonClass()
+        { }
+
+        public void Show()
+        {
+            Console.WriteLine("Hello " + i.ToString());
+            i += 1;
+        }
+
+        public static object SingletonObject
+        {
+            get
+            {
+                if (_singletonObject == null)
+                {
+                    lock (_syncRoot)
+                    {
+                        if (_singletonObject == null)
+                        {
+                            GetSingleton();
+
+                        }
+                    }
+                }
+
+                return _singletonObject;
+            }
+        }
+
+        private static void GetSingleton()
+        {
+            _singletonObject = new SingletonClass();
+        }
+    }
+}
