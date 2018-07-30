bf56aec3e54346f6d86174aacb07aee7d65c72fe
thunderstormwang <thunderstormwang@gmail.com>
2018-07-30 21:06:51 +0800

    修正 gitignore 位置 上傳 Facade 範例

---
+++ DesignPatternExample/FacadeExample.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadeExample
+{
+    public class MyJsonConver
+    {
+        public static string Serialize(object source)
+        {
+            return JsonConvert.SerializeObject(source);
+        }
+
+        public static T DeserializeObject<T>(string source)
+        {
+            return JsonConvert.DeserializeObject<T>(source);
+        }
+    }
+}
---
+++ DesignPatternExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using FacadeExample;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternExample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
