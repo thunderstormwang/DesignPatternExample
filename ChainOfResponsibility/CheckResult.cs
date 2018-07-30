1fcf2068f4c3b46a1ea2704813e26209309ea8bf
thunderstormwang <thunderstormwang@gmail.com>
2018-07-30 21:54:42 +0800

    Add ChainOfResponsibility partial

---
+++ ChainOfResponsibility/CheckResult.cs
@@ -0,0 +1,9 @@
+﻿namespace ChainOfResponsibility
+{
+    public class CheckResult
+    {
+        public string Source { get; set; }
+        public bool Result { get; set; }
+
+    }
+}
---
+++ ChainOfResponsibility/OriginalSample/FormatChecker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ChainOfResponsibility.OriginalSample
+{
+    public class FormatChecker
+    {
+        public CheckResult Check(string source)
+        {
+            // 巢狀 if, 不易讀且難維護
+
+            var result = new CheckResult() { Source = source };
+
+            if (source.Length == 29)
+            {
+                if (source.Substring(0, 3) == "965")
+                {
+                    DateTime firstDate;
+                    if (DateTime.TryParseExact(source.Substring(13, 8), "yyyyMMdd", System.Globalization.CultureInfo.InvariantCulture,
+                           System.Globalization.DateTimeStyles.None, out firstDate))
+                    {
+                        DateTime secondDate;
+                        if (DateTime.TryParseExact(source.Substring(21, 8), "yyyyMMdd", System.Globalization.CultureInfo.InvariantCulture,
+                           System.Globalization.DateTimeStyles.None, out secondDate))
+                        {
+                            result.Result = true;
+                        }
+
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
