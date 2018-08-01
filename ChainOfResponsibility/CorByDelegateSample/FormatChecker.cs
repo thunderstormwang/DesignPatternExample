eae891c778d609b766b6ff4e08c87a15d6bc939c
thunderstormwang <thunderstormwang@gmail.com>
2018-08-01 21:39:12 +0800

    Add ChainOfResponsibility

---
+++ ChainOfResponsibility/CorByDelegateSample/FormatChecker.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility.CorByDelegateSample
+{
+    public class FormatChecker
+    {
+        List<Func<string, bool>> _checkers;
+
+        public CheckResult Check(string source)
+        {
+            CreateCheckers();
+            var isErrorDetected = _checkers.Any((x) => x.Invoke(source) == false);
+            if (isErrorDetected)
+            {
+                return new CheckResult() { Source = source, Result = false };
+            }
+            else
+            {
+                return new CheckResult() { Source = source, Result = true };
+            }
+        }
+
+        private void CreateCheckers()
+        {
+            if (_checkers == null)
+            {
+                _checkers = new List<Func<string, bool>>();
+
+                _checkers.Add((x) => x.Length == 29);
+                _checkers.Add((x) =>
+                {
+                    string head = x.Substring(0, 3);
+                    return head == "965";
+                });
+                _checkers.Add((x) =>
+                {
+                    var dateString = x.Substring(13, 8);
+                    DateTime date;
+                    return DateTime.TryParseExact(dateString, "yyyyMMdd", System.Globalization.CultureInfo.InvariantCulture,
+                                   System.Globalization.DateTimeStyles.None, out date);
+                });
+                _checkers.Add((x) =>
+                {
+                    var dateString = x.Substring(21, 8);
+                    DateTime date;
+                    return DateTime.TryParseExact(dateString, "yyyyMMdd", System.Globalization.CultureInfo.InvariantCulture,
+                                   System.Globalization.DateTimeStyles.None, out date);
+                });
+            }
+        }
+    }
+}
--- SingletonSample/SingletonClass.cs
+++ SingletonSample/SingletonClass.cs
@@ -24,7 +24,7 @@ namespace SingletonSample
             i += 1;
         }
 
-        public static object SingletonObject
+        public static SingletonClass SingletonObject
         {
             get
             {
