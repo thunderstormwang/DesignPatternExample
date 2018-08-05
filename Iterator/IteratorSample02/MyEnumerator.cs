a3c1467a649eac8303a1db88beff85f6ce30fa85
thunderstormwang <thunderstormwang@gmail.com>
2018-08-05 11:17:26 +0800

    Add Iterator

---
+++ Iterator/IteratorSample02/MyEnumerator.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Iterator.IteratorSample02
+{
+    public class MyEnumerator : IEnumerator<double>
+    {
+        private List<double> _list = new List<double>();
+        private int _index = -1;
+
+        public MyEnumerator(List<double> list)
+        {
+            _list = list;
+        }
+
+        public double Current
+        {
+            get
+            {
+                return _list[_index];
+            }
+        }
+
+        public bool MoveNext()
+        {
+            _index++;
+            return _index >= _list.Count ? false : true;
+        }
+
+        public void Reset()
+        {
+            // 設定列舉值至它的初始位置, 在集合中第一個元素之前
+            _index = -1;
+        }
+
+        object IEnumerator.Current
+        {
+            // 因為 IEnumerator<T> 有繼承 IEnumerator, 你也得實作此方法
+            get
+            {
+                return null;
+            }
+        }
+
+        public void Dispose()
+        {
+            // 因為 IEnumerator<T> 有繼承 IDisposable, 你也得實作此方法
+        }
+    }
+}
---
+++ Iterator/Program.cs
@@ -0,0 +1,44 @@
+﻿using Iterator.IteratorSample01;
+using Iterator.IteratorSample02;
+using Iterator.IteratorSample03;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MyIterator01 myIterator01 = new MyIterator01();
+
+            foreach (double item in myIterator01)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+
+            MyIterator02 myIterator02 = new MyIterator02();
+
+            foreach (double item in myIterator02)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+
+            MyIterator03 myIterator03 = new MyIterator03();
+
+            foreach (double item in myIterator03)
+            {
+                Console.Write(item + " ");
+            }
+
+
+        }
+    }
+}
