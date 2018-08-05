2abde5b6f4831253f6c9ebe98e24b6fc66a89ce2
thunderstormwang <thunderstormwang@gmail.com>
2018-08-05 10:09:30 +0800

    Add AbstractFactory

---
+++ AbstractFactory/AbstractProductA.cs
@@ -0,0 +1,14 @@
+﻿namespace AbstractFactorySample
+{
+    /// <summary>
+    /// 產品 A 的一般化
+    /// </summary>
+    public abstract class AbstractProductA
+    { }
+
+    internal class ProductA1 : AbstractProductA
+    { }
+
+    internal class ProductA2 : AbstractProductA
+    { }
+}
---
+++ AbstractFactory/AbstractProductB.cs
@@ -0,0 +1,14 @@
+﻿namespace AbstractFactorySample
+{
+    /// <summary>
+    /// 產品 B 的一般化
+    /// </summary>
+    public abstract class AbstractProductB
+    { }
+
+    internal class ProductB1 : AbstractProductB
+    { }
+
+    internal class ProductB2 : AbstractProductB
+    { }
+}
