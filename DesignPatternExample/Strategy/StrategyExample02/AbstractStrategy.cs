767eafc066ce40d2bd2a5e75f7dfb927bdbd0cc9
thunderstormwang <thunderstormwang@gmail.com>
2018-08-09 21:49:46 +0800

    Add Strategy

---
+++ DesignPatternExample/Strategy/StrategyExample02/AbstractStrategy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Strategy.StrategyExample02
+{
+    public abstract class AbstractStrategy
+    {
+        public abstract void AlgorithmMethod();
+    }
+
+    internal class Strategy1 : AbstractStrategy
+    {
+        public override void AlgorithmMethod()
+        {
+            Console.WriteLine("Strategy 1");
+        }
+    }
+
+    internal class Strategy2 : AbstractStrategy
+    {
+        public override void AlgorithmMethod()
+        {
+            Console.WriteLine("Strategy 2");
+        }
+    }
+}
