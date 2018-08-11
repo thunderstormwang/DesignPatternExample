5423906b89e33add5bc079700a8ee7514057a642
thunderstormwang <thunderstormwang@gmail.com>
2018-08-11 10:48:30 +0800

    Add Command

---
+++ DesignPatternExample/Command/Invoker.cs
@@ -0,0 +1,17 @@
+﻿namespace Command
+{
+    public class Invoker
+    {
+        private Command _command;
+
+        public void SetCommnad(Command command)
+        {
+            _command = command;
+        }
+
+        public void Action()
+        {
+            _command.execute();
+        }
+    }
+}
---
+++ DesignPatternExample/Command/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Invoker invoker = new Invoker();
+            Receiver receiver = new Receiver();
+            CommandA command = new CommandA(receiver);
+            invoker.SetCommnad(command);
+            invoker.Action();
+
+            Console.ReadLine();
+        }
+    }
+}
