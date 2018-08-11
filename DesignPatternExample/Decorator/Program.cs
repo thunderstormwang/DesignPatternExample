689d0a827cc58fe0c58268fb1cd7e60220861506
thunderstormwang <thunderstormwang@gmail.com>
2018-08-11 10:09:34 +0800

    Add Decorator

---
+++ DesignPatternExample/Decorator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // 可層層堆疊
+            Component c = new ConcreteComponent();
+            Component da = new ConcreteDecoratorA(c);
+            Component db = new ConcreteDecoratorB(da);
+            db.Operation();
+
+            Console.ReadLine();
+
+        }
+    }
+}
