0f2c1c1b9039335a30a04f86fc73d022dc3d8c20
thunderstormwang <thunderstormwang@gmail.com>
2018-08-11 11:05:58 +0800

    Add Bridge

---
+++ DesignPatternExample/Bridge/IImplementor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Bridge
+{
+    public interface IImplementor
+    {
+        void OperationImp();
+    }
+
+    public class ImplementorA : IImplementor
+    {
+        public void OperationImp()
+        {
+            Console.WriteLine("Implementor A");
+        }
+    }
+
+    public class ImplementorB : IImplementor
+    {
+        public void OperationImp()
+        {
+            Console.WriteLine("Implementor B");
+        }
+    }
+}
---
+++ DesignPatternExample/Bridge/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            RefindAbstraction o = new RefindAbstraction(new ImplementorA());
+            o.Operation();
+
+            Console.ReadLine();
+
+        }
+    }
+}
